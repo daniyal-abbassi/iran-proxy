e6a6151e5c6c12617b80a84cb3f38d7353a0ef45
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-06 17:50:28 +0330

    chore: set-up cheerio & anxios

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 //require cheerio and axios
+const cheerio = require("cheerio");
+const axios = require("axios");
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World!")
