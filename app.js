a501819b39fdcf1dc7079613d5ad9fec19bf4ddb
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-07 19:18:12 +0330

    chore: install puppeteer

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-//require cheerio and axios
-const cheerio = require("cheerio");
-const axios = require("axios");
 
 
-app.get("/",(req,res)=>{
+
+
+app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
-app.listen(3001,()=>{console.log("Server is Running on Port: 3001")})
+app.listen(3001, () => { console.log("Server is Running on Port: 3001") })
 
