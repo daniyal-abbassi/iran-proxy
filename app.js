31335667fd9ade89824c5c201d04bd5fb9d6dc59
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-10 17:32:28 +0330

    feat: add another proxy repo

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 //set view engine
-app.use(express.urlencoded({extended: true}));
-app.set("view engine","ejs");
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 //require axios
 const axios = require('axios');
 //require cheerio
@@ -17,7 +17,7 @@ const config = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     },
 };
- const proxyList = [];
+const FinalproxyList = [];
 //async function
 async function scraping() {
     try {
@@ -27,7 +27,7 @@ async function scraping() {
         const $ = cheerio.load(res.data);
         //extract some text 
         const $proxyTable = $('tbody').children('tr');
-       
+        let proxyList = [];
         $proxyTable.map((i, row) => {
             let ip = $(row).children().eq(0).text();
             let port = $(row).children().eq(1).text();
@@ -38,11 +38,11 @@ async function scraping() {
             }
             proxyList.push({
                 ipAddress: ip,
-                port: Number(port),
+                port: port,
                 protocol: protocol,
             })
         })
-
+        FinalproxyList.push(...proxyList)
     } catch (error) {
         console.error('Error: %s', error)
     }
@@ -51,23 +51,31 @@ async function scraping() {
 // scraping()
 async function scraping1() {
     try {
-        const res = await axios.get(siteProxy1,config);
+        const res = await axios.get(siteProxy1, config);
         const $ = cheerio.load(res.data);
-        const $testIp = $('td > abbr').text();
-        console.log($testIp)
+        const $testIp = $('td > abbr');
+        let proxyList = [];
+        $testIp.map((i, el) => {
+            proxyList.push($(el).attr('title'))
+        })
+        let proxyList1 = []
+        for (let i = 0; i < proxyList.length; i += 2) {
+            proxyList1.push({ ipAddress: proxyList[i], port: proxyList[i + 1] })
+        }
+        FinalproxyList.push(...proxyList1)
     } catch (error) {
-        console.error('ERROR: %s',error)
+        console.error('ERROR: %s', error)
     }
 }
-
+scraping()
 scraping1()
 
 
 
 
 app.get("/", (req, res) => {
-    // res.render('list',{proxyList})
-    res.send('Hello world!!')
+    res.render('list',{FinalproxyList})
+    // res.send('Hello world!!')
 })
 
 app.listen(3001, () => { console.log("Server is Running on Port: 3001") })
