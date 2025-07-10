d73ef6fde9c3fc035bc9e3f39e242a421e464c1c
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-10 16:20:41 +0330

    chore: setup ejs viewEngine

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+//set view engine
+app.use(express.urlencoded({extended: true}));
+app.set("view engine","ejs");
 //require axios
 const axios = require('axios');
 //require cheerio
@@ -13,7 +16,7 @@ const config = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     },
 };
-
+ const proxyList = [];
 //async function
 async function scraping() {
     try {
@@ -23,13 +26,13 @@ async function scraping() {
         const $ = cheerio.load(res.data);
         //extract some text 
         const $proxyTable = $('tbody').children('tr');
-        const proxyList = [];
+       
         $proxyTable.map((i, row) => {
             let ip = $(row).children().eq(0).text();
             let port = $(row).children().eq(1).text();
             let protocol = $(row).children().eq(3).text();
             // let date = $(row).children().eq(-1).text();
-            if(!protocol) {
+            if (!protocol) {
                 return
             }
             proxyList.push({
@@ -38,9 +41,7 @@ async function scraping() {
                 protocol: protocol,
             })
         })
-        console.log(proxyList)
-      
-       
+
     } catch (error) {
         console.error('Error: %s', error)
     }
@@ -54,7 +55,7 @@ scraping()
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send(proxyList)
 })
 
 app.listen(3001, () => { console.log("Server is Running on Port: 3001") })
