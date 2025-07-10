e7f9959d21b0c2e0e1dd5065c270ffaebc1e9539
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-10 16:02:08 +0330

    fix: delete proxy-verifier

--- app.js
+++ app.js
@@ -2,45 +2,47 @@ const express = require("express");
 const app = express();
 //require axios
 const axios = require('axios');
-//requre cheerio
+//require cheerio
 const cheerio = require('cheerio');
+
 //example site
-let siteImdb = 'https://www.imdb.com/title/tt1392170/';
-let siteWiki = 'https://en.wikipedia.org/wiki/Web_scraping';
 let siteProxy = 'https://freeproxyupdate.com/iran-ir';
 //axios config options
-const config = { 
-	headers: { 
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36', 
-	}, 
-}; 
- 
+const config = {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    },
+};
+
 //async function
 async function scraping() {
     try {
         //fetch the site
-        const res = await axios.get(siteProxy,config);
+        const res = await axios.get(siteProxy, config);
         //lead html to cheerio
         const $ = cheerio.load(res.data);
         //extract some text 
         const $proxyTable = $('tbody').children('tr');
         const proxyList = [];
-        $proxyTable.map((i,row) => {
+        $proxyTable.map((i, row) => {
             let ip = $(row).children().eq(0).text();
             let port = $(row).children().eq(1).text();
             let protocol = $(row).children().eq(3).text();
-            let date = $(row).children().eq(-1).text();
-            // console.log(`ip: ${ip}:${port}\n${protocol} - ${date}`)
+            // let date = $(row).children().eq(-1).text();
+            if(!protocol) {
+                return
+            }
             proxyList.push({
-                ip: ip,
-                port: port,
+                ipAddress: ip,
+                port: Number(port),
                 protocol: protocol,
-                date: date
             })
         })
         console.log(proxyList)
+      
+       
     } catch (error) {
-        console.error('Error: %s',error)
+        console.error('Error: %s', error)
     }
 }
 
