d0824a59af6f9070d17ac85a54ed8a5afae8dcc9
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-13 17:51:31 +0330

    feat: install and setup proxy-checker-agents

---
+++ proxy_checker.js
@@ -0,0 +1,5 @@
+//PROXY CHECKER
+//import dependencies
+const axios = require('axios');
+const {SocksProxyAgent} = require('socks-proxy-agent');
+const HttpsProxyAgent = require('https-proxy-agent');
