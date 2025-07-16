94a021c1b55045a61feb58123b6e2aad471ede1b
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-16 16:02:19 +0330

    feat: set-up new approach and folder-structure

--- app.js
+++ app.js
@@ -79,7 +79,6 @@ async function scraping1() {
 
 const pup = require('puppeteer');
 const url = 'https://www.ditatompel.com/proxy/country/ir';
-const newURL = 'https://proxyhub.me/en/ir-free-proxy-list.html'
 // create an async function
 async function scrape() {
     try {
@@ -115,57 +114,6 @@ async function scrape() {
         console.log('scrape failed: ', error);
     }
 }
-
-const monsterUrl = "https://getfreeproxy.com/db/country/IR?page=1&protocol=http&country_code=IR&country_name=Iran%2C+Islamic+Republic+of"
-//with puppetter methode
-async function scrapeMonster() {
-    //create a browser
-    const browser = await pup.launch({ headless: true });
-    //create a page
-    const page = await browser.newPage();
-
-    //create timeout 
-    const PAGE_LOAD_TIMEOUT = 30000; // 30 seconds
-    const DELAY_BETWEEN_PAGES = Math.floor(Math.random() * 5000) + 2000; // 2-7s; 
-    try {
-        console.log('we are scraping from ' + monsterUrl + ":");
-        //go to the page
-        await page.goto(monsterUrl);
-        //get the total page number to rotate
-        let totalPageNumber = await page.$eval('span.font-medium:nth-child(4)', el => el.textContent)
-        //loop through pages
-        for (let i = 1; i < 10; i++) {
-            console.log(`\nSCRAPING FROM PAGE ${i}...`)
-            //go to desired page number
-            await page.goto(`https://getfreeproxy.com/db/country/IR?page=${i}&protocol=http&country_code=IR&country_name=Iran%2C+Islamic+Republic+of`, {
-                waitUntil: 'domcontentloaded',
-                timeout: PAGE_LOAD_TIMEOUT
-            }).catch(() => { throw new Error(`Timeout loading page ${i}`); });
-            //get the proxies
-            let table = await page.evaluate(() => {
-                let tableData = Array.from(document.querySelectorAll('tr td a'), (a) => {
-                    let line = a.textContent;
-                    return line.trim()
-                }
-                ).filter(str => str !== '').map(url => url.split('://')[1]);
-                return tableData
-            })
-            console.log(`PROXIES OF PAGE ${i}: `)
-            console.log(table)
-            //wait 5s before goes to next page
-            console.log(`Waiting ${DELAY_BETWEEN_PAGES / 1000}s...`);
-            for (let j = Math.round(DELAY_BETWEEN_PAGES / 1000); j > 0; j--) {
-                await new Promise(resolve => setTimeout(resolve, 1000));
-                process.stdout.write(`${j}...`)
-            }
-            console.log('\n')
-        }
-
-        await browser.close()
-    } catch (error) {
-        console.error('scraping failed: ', error)
-    }
-};
 
 
 //scrape from https://getfreeproxy.com
