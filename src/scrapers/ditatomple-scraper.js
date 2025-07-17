b8caa2f8c1bd38223c60de9ae9e4b51e06c60a46
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-17 13:51:37 +0330

    fix: modify scrape to try 3 times

--- src/scrapers/ditatomple-scraper.js
+++ src/scrapers/ditatomple-scraper.js
@@ -5,7 +5,7 @@ const pup = require('puppeteer');
 // --- URL ---
 const URL = 'https://www.ditatompel.com/proxy/country/ir';
 
-const USER_AGENT =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 // create an async function
 async function scrapeDitatomper() {
     let browser;
@@ -29,31 +29,42 @@ async function scrapeDitatomper() {
             waitUntil: 'domcontentloaded', // Changed from array to string
             timeout: 45000
         });;
-        // //get the table/tr/td/strong/span and... test
-        //changing select
-        await page.waitForSelector('select#rowsPerPage', {
-            timeout: 10000
-        })
-        await page.select('select#rowsPerPage', '100');
-        //wait for changes
-        await page.waitForNetworkIdle();
-        let table = await page.evaluate(() => {
-            let tableData = Array.from(document.querySelectorAll('td  strong'), (strong) => strong.textContent);
-            return tableData
-        })
-     
-        const proxies = [];
-        for (let i = 0; i < table.length; i++) {
-            let [ip, port] = table[i].split(':');
-            proxies.push(`${ip}:${port}`)
-        }
-        console.log(proxies)
-        return proxies
+        //retry 3 times to get all pages
+        let maxTetries = 3;
+        let retryCount = 0;
+        //retry scraping to get all pages
+        while (retryCount < maxTetries) {
+            try {
+                //changing select
+                await page.waitForSelector('select#rowsPerPage', {
+                    timeout: 10000
+                })
+                await page.select('select#rowsPerPage', '100');
+                //wait for changes
+                await page.waitForNetworkIdle();
+                let proxies = await page.evaluate(() => {
+                    let tableData = Array.from(document.querySelectorAll('td  strong'), (strong) => strong.textContent);
+                    return tableData
+                })
+                //check for length
+                if (proxies.length >= 20) {
+                    return proxies;
+                }
+
+                retryCount++;
+                console.log(`Retry ${retryCount}: Only got ${proxies.length} items`);
+
+            } catch (error) {
+                retryCount++;
+                console.log(`Retry ${retryCount} failed:`, error.message);
+            }
+        } // while loop
+
     } catch (error) {
-        console.log('scrape failed: ', error);
+        console.log('scrape failed: ', error.message);
     } finally {
         await browser.close();
     }
 }
 
-scrapeDitatomper()
+module.exports = { scrapeDitatomper }
